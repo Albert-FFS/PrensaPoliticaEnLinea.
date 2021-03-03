b6ac796ed814ac2aac850e65cf2b2a9ded769531
Albert-FFS <76725661+Albert-FFS@users.noreply.github.com>
2021-03-03 00:22:04 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*function GetterPageDimension(){
+    const WPage =window.screen.width;
+    const HPage =window.screen.height;
+    DimensionText ='Ancho es de : '+ WPage +' y el Alto es de : '+ HPage;
+    //Texto.innerHTML =DimensionText;
+}
+*/
